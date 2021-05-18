853d5c5a330aa6c6a2338b0c71368ab3c7c94e5b
milanoid <milanvojnovic@gmail.com>
2021-05-18 17:05:03 +0200

    assertions using chai

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,9 +1,10 @@
 const puppeteer = require('puppeteer')
+const expect = require('chai').expect
 
 describe('My First Puppeteer Test', () => {
     it('should launch the browser', async function(){
         const browser = await puppeteer.launch({
-            headless: false, 
+            headless: true, 
             slowMo: 10, 
             devtools: false, 
         })
@@ -11,11 +12,17 @@ describe('My First Puppeteer Test', () => {
         await page.goto('https://example.com/')
         const actualTitle = await page.title()
         const actualUrl = await page.url()
-        // $eval(<element>, <callback function>)
+        // $eval(<element>, <callback function>) returns one element
         const actualText = await page.$eval('h1', element => element.textContent)
+        // $$eval returns multiple elements
         const actualCount = await page.$$eval('p', element => element.length)
-        console.log('Text in the H1: ' + actualTitle)
-        console.log('Actual number of <p> tags: ' + actualCount)
+
+        // assertions using chai
+        expect(actualTitle).to.be.a('string', 'Example Domain')
+        expect(actualUrl).to.include('example.com')
+        expect(actualText).to.be.a('string', 'Example Domain')
+        expect(actualCount).to.eq(2)
+
         await browser.close()
     })
 })
