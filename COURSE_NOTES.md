55a102baeb48c9bc61ff35f6b8a8dd9c28c30af0
milanoid <milanvojnovic@gmail.com>
2021-05-19 07:57:09 +0200

    makes it prettier by npx prettier --write .

--- COURSE_NOTES.md
+++ COURSE_NOTES.md
@@ -1,11 +1,16 @@
 ## Section 1 - 4. Project setup
+
 - initialize by `npm init` and follow wizard
 - add dependencies Puppeteer (automation library), Mocha (test runner), Chai (assertion library)
-    `npm install puppeteer mocha chai`
+  `npm install puppeteer mocha chai`
+
 ## Section 1 - 5. Project setup - prettier
+
 - `npm install prettier`
 - edit `.prettierrc` configuration file
+- run `npx prettier --write .` to apply formatting
 
 ## Section 2 - Launch & Close browser
+
 - `example.test.js` + edit `package.json` - `scripts.test`
 - run tests by `npm run test`
--- tests/example.test.js
+++ tests/example.test.js
@@ -2,39 +2,39 @@ const puppeteer = require('puppeteer')
 const expect = require('chai').expect
 
 describe('My First Puppeteer Test', () => {
-    it('should launch the browser', async function(){
-        const browser = await puppeteer.launch({
-            headless: false, 
-            slowMo: 10, 
-            devtools: false, 
-        })
-        const page = await browser.newPage()
-        await page.setDefaultTimeout(10000) // by default it is 30 seconds
-        await page.setDefaultTimeout(20000)
-        await page.goto('https://example.com/')
-        await page.waitForXPath('//h1')
+	it('should launch the browser', async function () {
+		const browser = await puppeteer.launch({
+			headless: false,
+			slowMo: 10,
+			devtools: false,
+		})
+		const page = await browser.newPage()
+		await page.setDefaultTimeout(10000) // by default it is 30 seconds
+		await page.setDefaultTimeout(20000)
+		await page.goto('https://example.com/')
+		await page.waitForXPath('//h1')
 
-        const actualTitle = await page.title()
-        const actualUrl = await page.url()
-        // $eval(<element>, <callback function>) returns one element
-        const actualText = await page.$eval('h1', element => element.textContent)
-        // $$eval returns multiple elements
-        const actualCount = await page.$$eval('p', element => element.length)
+		const actualTitle = await page.title()
+		const actualUrl = await page.url()
+		// $eval(<element>, <callback function>) returns one element
+		const actualText = await page.$eval('h1', element => element.textContent)
+		// $$eval returns multiple elements
+		const actualCount = await page.$$eval('p', element => element.length)
 
-        // assertions using chai
-        expect(actualTitle).to.be.a('string', 'Example Domain')
-        expect(actualUrl).to.include('example.com')
-        expect(actualText).to.be.a('string', 'Example Domain')
-        expect(actualCount).to.eq(2)
+		// assertions using chai
+		expect(actualTitle).to.be.a('string', 'Example Domain')
+		expect(actualUrl).to.include('example.com')
+		expect(actualText).to.be.a('string', 'Example Domain')
+		expect(actualCount).to.eq(2)
 
-        await page.goto('http://zero.webappsecurity.com/index.html')
-        await page.waitForSelector('#signin_button')
-        await page.click('#signin_button')
-        await page.waitForFunction(() => !document.querySelector('#signin_button'))
-        await page.waitForSelector('#signin_button', { 
-            hidden: true, 
-            timeout: 3000 
-        })
-        await browser.close()
-    })
+		await page.goto('http://zero.webappsecurity.com/index.html')
+		await page.waitForSelector('#signin_button')
+		await page.click('#signin_button')
+		await page.waitForFunction(() => !document.querySelector('#signin_button'))
+		await page.waitForSelector('#signin_button', {
+			hidden: true,
+			timeout: 3000,
+		})
+		await browser.close()
+	})
 })
